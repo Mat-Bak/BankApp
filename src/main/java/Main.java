caa368887b54d5270200bd9ffa8bd803f9642760
Mateusz Bak <matbak4@gmail.com>
2022-11-24 19:02:04 +0100

    delete file

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Mateusz
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        NewJPanel N = new NewJPanel();
+        N.setPreferredSize(new Dimension(500,500));
+        //N.setMinimumSize(new Dimension(650,470));
+        N.dupa();
+       
+    }
+    
+       
+}
