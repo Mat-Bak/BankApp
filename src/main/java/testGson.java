e538654097e5e356242eec4015952fb96514338c
Mateusz <matbak4@gmail.com>
2023-01-12 23:05:30 +0100

    Login to exist account

--- src/main/java/testGson.java
+++ src/main/java/testGson.java
@@ -1,8 +1,18 @@
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.Writer;
+import java.lang.reflect.Type;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,30 +26,49 @@ import java.util.List;
  * @author Mateusz
  */
 
-// name,  surname,  city,  address,  login,  password,  sex,  email,  phoneNumber,  pesel,  postCode  
+//  PLIK DO TESTOWANIA
 
 public class testGson {
-    public static void main(String[] args) {
+    
+    /**
+     *
+     */
+   // final static File file = new File(getClass().getResource("accounts.json").getPath());
+    //Path path = Path.of("bankapp", "src", "main","java","accounts.json");
+    final static String outputFilePath = "\\src\\main\\java\\accounts.json";
+            //D:\Własne\Programowanie\Repozytorium\bankapp\src\main\java\testGson.java
+    //D:\Własne\Programowanie\Repozytorium\bankapp\src\main\java\accounts.json
+    
+    public static List<Account> loadData(){
+        
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString() + outputFilePath;
+        
+        List<Account> data = null;
         try {
-    // create a list of users
-    List<Account> users = Arrays.asList(
-            new Account("Adam", "Doe", "Poznan", "Kolorowa 12", "tajnyLogin", "!1234Qwer", "Male", "john.doe@example.com", "458785458", "45898565487", "85478"),
-            new Account("Bob", "Sue", "Krakov", "srednia 122", "toJestLogin", "!1234Qwer", "Female", "Bob.doe@example.com", "525252525", "52332565232", "854-74")
-    );
+           File file = new File(s);
+           Gson gson = new Gson();
+           BufferedReader bf = new BufferedReader(new FileReader(file));
 
-    // create writer
-    Writer writer = new FileWriter("D:\\Własne\\Programowanie\\Repozytorium\\bankapp\\src\\main\\java\\accounts.json");
+            Type userType = new TypeToken<List<Account>>(){}.getType(); 
+            data = gson.fromJson(bf, userType);
+        
+        } catch (JsonIOException | JsonSyntaxException | FileNotFoundException e) {
+            System.out.println("ERROR DATA LOAD!");
+        }
+        
+        return data;
+    }
+    
+    public static void main(String[] args) {
 
-    // convert users list to JSON file
-    Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    gson.toJson(users, writer);
+        List<Account> users = loadData();
+        
+        for(Account user : users){
+            System.out.println("User: " + user.getName());
+        }
 
-    // close writer
-    writer.close();
-
-} catch (Exception ex) {
-    ex.printStackTrace();
-}
+       
     }
     
 }
